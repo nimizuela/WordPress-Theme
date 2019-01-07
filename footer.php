05299ef35df218b081539d4355f25f193e1a9d9d
Developer <>
2019-01-07 11:51:19 -0400

    update contact email

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 				<li><a href="https://twitter.com/nimizuela" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 				<li><a href="https://www.discord.gg/XtFU7qh" class="icon fa-discord"><span class="label">Discord</span></a></li>
 				<li><a href="https://github.com/nimizuela" class="icon fa-github"><span class="label">Github</span></a></li>
-				<li><a href="mailto:nimizuela@protonmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+				<li><a href="mailto:<?= get_bloginfo('admin_email'); ?>" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
 			</ul>
 			<ul class="copyright">
 				<li>&copy; <?= get_bloginfo('name'); ?></li><li><?= pll__('Design'); ?>: <a href="http://html5up.net">HTML5 UP</a></li>
